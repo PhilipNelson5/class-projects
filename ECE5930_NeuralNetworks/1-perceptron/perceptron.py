ac1275a9b9361e133f4048988950d9f1b6a744ce
Philip Nelson <PhilipNelson5@gmail.com>
2017-08-29 17:44:01 -0600

    training works

--- ECE5930_NeuralNetworks/1-perceptron/perceptron.py
+++ ECE5930_NeuralNetworks/1-perceptron/perceptron.py
@@ -1,11 +1,42 @@
 from graphics import *
 import random
+import time
 
-height = 500
-width = 500
+height = 700
+width = 700
+MIN = -5
+MAX = 5
 POINT = 0
 LABEL = 1
+X = 0
+Y = 1
 win = GraphWin("Drawing Test", height, width)
+
+def interp(x, x1 = -5, x2 = 5, y1 = 0, y2 = height):
+    return y1 + ((x-x1) * (y2-y1) / (x2-x1))
+
+def plot(x, y, outline, fill, size):
+    x = interp(x, -5, 5, 0, width)
+    y = interp(y, -5, 5, 0, height)
+    cir = Circle(Point(x, y), size)
+    cir.setOutline(outline)
+    cir.setFill(fill)
+    cir.draw(win)
+
+def loadData(file, target, color, dataSet = []):
+    for line in open(file,"r"):
+        point = line.split()
+        point = [float(point[0]), float(point[1])]
+        plot(point[0], point[1], color, "white", 6)
+        dataSet.append([point, target])
+    return dataSet
+
+#    for i in range(0,550,50):
+#        line = Line(Point(i, 500), Point(500-i, 0))
+#        line.draw(win)
+#        win.getKey()   # Pause until key press to view result
+#        line.undraw()
+
 
 class Perceptron:
     def __init__(self, numInputs):
@@ -24,50 +55,35 @@ class Perceptron:
         return self.activationFunc(sum)
 
     def train(self, inputs, target):
-        #guess = self.guess(inputs)
         error = target - self.guess(inputs)
+        if error == 0:
+            plot(inputs[X], inputs[Y], "green", "green", 3)
+        else :
+            plot(inputs[X], inputs[Y], "red", "red", 3)
 
         for i in range(len(self.weights)):
             self.weights[i] += error * inputs[i] * self.lr
-         
-
-
-def interp(x, x1, x2, y1, y2):
-    return y1 + ((x-x1) * (y2-y1) / (x2-x1))
-
-def plot(x, y, color):
-    x = interp(x, -5, 5, 0, width)
-    y = interp(y, -5, 5, 0, height)
-    cir = Circle(Point(x, y), 3)
-    cir.setOutline(color)
-    cir.draw(win)
-
-def loadData(file, target, color):
-    dataSet = []
-    for line in open(file,"r"):
-        point = line.split()
-        point = [float(point[0]), float(point[1])]
-        plot(point[0], point[1], color)
-        dataSet.append([point, target])
-    return dataSet
-
-#    for i in range(0,550,50):
-#        line = Line(Point(i, 500), Point(500-i, 0))
-#        line.draw(win)
-#        win.getKey()   # Pause until key press to view result
-#        line.undraw()
-
 
 def main():
 
     win.setBackground("white")
 
-    set1 = loadData("perceptrondat1", 1, "blue")
-    set2 = loadData("perceptrondat2", -1,"black")
+    trainingData = loadData("perceptrondat1", 1, "black")
+    trainingData = loadData("perceptrondat2", -1,"black", trainingData)
 
     p = Perceptron(2)
 
-    p.train(set1[0][POINT], set1[0][LABEL])
+    #for i in range(len(trainingData)):
+    index = 0;
+    line = Line(Point(interp(-4), interp(-4)), Point(interp(4), interp(4)))
+    line.draw(win)
+    while True:
+        p.train(trainingData[index][POINT], trainingData[index][LABEL])
+        index += 1
+        index %= len(trainingData)
+        time.sleep(.01)
+
+
 
     win.getKey()   # Pause until key press to view result
     win.close()    # Close window when done
