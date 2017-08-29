d09ce65e1564e786818bce71f20e8bc5fc5ab1ee
Philip Nelson <PhilipNelson5@gmail.com>
2017-08-29 10:23:57 -0600

    added training

--- ECE5930_NeuralNetworks/1-perceptron/perceptron.py
+++ ECE5930_NeuralNetworks/1-perceptron/perceptron.py
@@ -3,10 +3,13 @@ import random
 
 height = 500
 width = 500
+POINT = 0
+LABEL = 1
 win = GraphWin("Drawing Test", height, width)
 
 class Perceptron:
     def __init__(self, numInputs):
+        self.lr = 0.1 # Learning Rate
         self.weights = [0 for i in range(numInputs)]
         for i in range (numInputs):
             self.weights[i] = random.uniform(-1, 1)
@@ -19,6 +22,13 @@ class Perceptron:
         for i in range(len(self.weights)):
             sum += inputs[i]*self.weights[i]
         return self.activationFunc(sum)
+
+    def train(self, inputs, target):
+        #guess = self.guess(inputs)
+        error = target - self.guess(inputs)
+
+        for i in range(len(self.weights)):
+            self.weights[i] += error * inputs[i] * self.lr
          
 
 
@@ -32,13 +42,13 @@ def plot(x, y, color):
     cir.setOutline(color)
     cir.draw(win)
 
-def loadData(file, color):
+def loadData(file, target, color):
     dataSet = []
     for line in open(file,"r"):
         point = line.split()
         point = [float(point[0]), float(point[1])]
         plot(point[0], point[1], color)
-        dataSet.append(point)
+        dataSet.append([point, target])
     return dataSet
 
 #    for i in range(0,550,50):
@@ -52,13 +62,12 @@ def main():
 
     win.setBackground("white")
 
-    set1 = loadData("perceptrondat1", "blue")
-    set2 = loadData("perceptrondat2", "black")
+    set1 = loadData("perceptrondat1", 1, "blue")
+    set2 = loadData("perceptrondat2", -1,"black")
 
     p = Perceptron(2)
 
-    for i in range(len(set1)):
-        print(p.guess(set1[i]))
+    p.train(set1[0][POINT], set1[0][LABEL])
 
     win.getKey()   # Pause until key press to view result
     win.close()    # Close window when done
