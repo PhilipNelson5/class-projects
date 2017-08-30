3bc808920f96b00c882da696874d21cae535ba4e
Philip Nelson <PhilipNelson5@gmail.com>
2017-08-29 20:13:27 -0600

    Perceptron completed, and line visulation

--- ECE5930_NeuralNetworks/1-perceptron/perceptron.py
+++ ECE5930_NeuralNetworks/1-perceptron/perceptron.py
@@ -4,20 +4,31 @@ import time
 
 height = 700
 width = 700
-MIN = -5
-MAX = 5
-POINT = 0
-LABEL = 1
+minX = -5
+maxX = 5
+minY = -5
+maxY = 5
 X = 0
 Y = 1
 win = GraphWin("Drawing Test", height, width)
 
-def interp(x, x1 = -5, x2 = 5, y1 = 0, y2 = height):
+class MyPoint:
+    def __init__(self, x_, y_, b_= 1, l_= 0):
+        self.x = x_     # x coordinate
+        self.y = y_     # y coordinate
+        self.b = b_     # bias
+        self.label = l_ # label of data set
+
+    def mapX(self):
+        return interp(self.x, minX, maxX, 0, width)
+
+    def mapY(self):
+        return interp(self.y, minY, maxY, height, 0)
+
+def interp(x, x1, x2, y1, y2):
     return y1 + ((x-x1) * (y2-y1) / (x2-x1))
 
 def plot(x, y, outline, fill, size):
-    x = interp(x, -5, 5, 0, width)
-    y = interp(y, -5, 5, 0, height)
     cir = Circle(Point(x, y), size)
     cir.setOutline(outline)
     cir.setFill(fill)
@@ -25,18 +36,11 @@ def plot(x, y, outline, fill, size):
 
 def loadData(file, target, color, dataSet = []):
     for line in open(file,"r"):
-        point = line.split()
-        point = [float(point[0]), float(point[1])]
-        plot(point[0], point[1], color, "white", 6)
-        dataSet.append([point, target])
+        raw = line.split()
+        p = MyPoint(float(raw[0]), float(raw[1]), 1, target)
+        plot(p.mapX(), p.mapY(), color, "white", 6)
+        dataSet.append(p)
     return dataSet
-
-#    for i in range(0,550,50):
-#        line = Line(Point(i, 500), Point(500-i, 0))
-#        line.draw(win)
-#        win.getKey()   # Pause until key press to view result
-#        line.undraw()
-
 
 class Perceptron:
     def __init__(self, numInputs):
@@ -44,6 +48,7 @@ class Perceptron:
         self.weights = [0 for i in range(numInputs)]
         for i in range (numInputs):
             self.weights[i] = random.uniform(-1, 1)
+        self.done = True
     
     def activationFunc(self, sum):
         return 1 if sum >= 0 else -1
@@ -54,12 +59,21 @@ class Perceptron:
             sum += inputs[i]*self.weights[i]
         return self.activationFunc(sum)
 
+    def guessY(self, x):
+        w0 = self.weights[0]
+        w1 = self.weights[1]
+        w2 = self.weights[2]
+
+        return -(w2/w1) - (w0/w1) * x
+
     def train(self, inputs, target):
         error = target - self.guess(inputs)
+        pt = MyPoint(inputs[X], inputs[Y])
         if error == 0:
-            plot(inputs[X], inputs[Y], "green", "green", 3)
+            plot(pt.mapX(), pt.mapY(), "green", "green", 3)
         else :
-            plot(inputs[X], inputs[Y], "red", "red", 3)
+            plot(pt.mapX(), pt.mapY(), "red", "red", 3)
+            self.done = False
 
         for i in range(len(self.weights)):
             self.weights[i] += error * inputs[i] * self.lr
@@ -71,17 +85,34 @@ def main():
     trainingData = loadData("perceptrondat1", 1, "black")
     trainingData = loadData("perceptrondat2", -1,"black", trainingData)
 
-    p = Perceptron(2)
+    p = Perceptron(3)
 
     #for i in range(len(trainingData)):
     index = 0;
-    line = Line(Point(interp(-4), interp(-4)), Point(interp(4), interp(4)))
-    line.draw(win)
+    iters = 0;
     while True:
-        p.train(trainingData[index][POINT], trainingData[index][LABEL])
+        ptA = MyPoint(minX,p.guessY(minX))
+        ptB = MyPoint(maxX,p.guessY(maxX))
+        line = Line(Point(ptA.mapX(), ptA.mapY()), Point(ptB.mapX(), ptB.mapY()))
+        line.draw(win)
+
+        x = trainingData[index].x
+        y = trainingData[index].y
+        b = trainingData[index].b
+        l = trainingData[index].label
+        p.train([x,y,b], l)
+
         index += 1
-        index %= len(trainingData)
-        time.sleep(.01)
+        if index == len(trainingData):
+            index = 0
+            iters += 1
+            if p.done:
+                print("iterations: " + str(iters))
+                break
+            p.done = True
+
+        time.sleep(.05)
+        line.undraw()
 
 
 
