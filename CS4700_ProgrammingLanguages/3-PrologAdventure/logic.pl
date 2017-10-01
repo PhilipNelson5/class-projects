c45d48f810621e1dad131b4b3b3eaba9bdf53d58
Philip Nelson <PhilipNelson5@gmail.com>
2017-09-30 18:40:52 -0600

    COLORS

--- CS4700_ProgrammingLanguages/3-PrologAdventure/logic.pl
+++ CS4700_ProgrammingLanguages/3-PrologAdventure/logic.pl
@@ -1,11 +1,23 @@
 :-include(adventure).
+reset   :- write("\033[0m").
+black   :- write("\033[30m").
+red     :- write("\033[31m").
+green   :- write("\033[32m").
+yellow  :- write("\033[33m").
+blue    :- write("\033[34m").
+magenta :- write("\033[35m").
+cyan    :- write("\033[36m").
+white   :- write("\033[37m").
+bold    :- write("\033[1m").
+clear   :- write("\033[2J").
+
 connected(X,Y) :- door(X,Y).
 connected(X,Y) :- door(Y,X).
 
 look(Place):-room(Place), descriptionShort(Place), listConnecions(Place), listItems(Place), !.
 look(_).
 
-search(Object):-container(Object), printName(Object), write(": "), long_desc(Object, Descript), write(Descript), nl, write("contains:\n"), listContainter(Object), !.
+search(Object):-container(Object), printName(Object), write(": "), long_desc(Object, Descript), write(Descript), nl, magenta, write("\nContains:\n"), reset, listContainter(Object), !.
 search(Object):-location(Object, _), long_desc(Object, Descript), write(Descript), nl, fail.
 search(_).
 
@@ -14,7 +26,7 @@ listContainter(Container):-location(Item, Container), descriptionLong(Item), nl,
 listContainter(_).
 
 /* List connections from a room */
-listConnecions(Place):-room(Place), write("\n\nConnections:\n"), connected(Place,X), descriptionShort(X), nl, fail.
+listConnecions(Place):-room(Place), cyan, write("\n\nConnections:\n"), reset, connected(Place,X), descriptionShort(X), nl, fail.
 listConnecions(_).
 
 /* Display the long description of a room */
@@ -26,7 +38,9 @@ descriptionShort(Place):- printName(Place), write(": "), short_desc(Place, Descr
 descriptionShort(_).
 
 /* List the items in a room */
-listItems(Place):-room(Place), write("\nItems:\n"), location(Item, Place), printName(Item), nl, fail.
+listItems(Place):-room(Place), red, write("\nItems:\n"), reset, location(Item, Place), descriptionShort(Item), nl, fail.
 listItems(_).
 
-printName(Thing):- name(Thing, Name), write(Name), !.
+printName(Thing):- name(Thing, Name), green, write(Name), reset, !.
+
+
