4fc77b097e2d14ce5f4eba02f7b1ab5b7375b98e
Philip Nelson <PhilipNelson5@gmail.com>
2017-11-08 09:52:21 -0700

    working on the printing

--- CS4700_ProgrammingLanguages/4-HaskellRedBlackTree/redBlackTree.hs
+++ CS4700_ProgrammingLanguages/4-HaskellRedBlackTree/redBlackTree.hs
@@ -12,7 +12,10 @@ insert x (Node c left a right)
   | x > a  = Node c left a (balance(insert x right))
 
 balance :: Tree a -> Tree a
+balance (Node Black (Node Red (Node Red a x b) y c ) z d) = (Node Red (Node Black a x b) y (Node Black c z d))
 balance (Node Black (Node Red a x (Node Red b y c)) z d) = (Node Red (Node Black a x b) y (Node Black c z d))
+balance (Node Black a x (Node Red (Node Red b y c) z d)) = (Node Red (Node Black a x b) y (Node Black c z d))
+balance (Node Black a x (Node Red b y (Node Red c z d))) = (Node Red (Node Black a x b) y (Node Black c z d))
 balance t = t
 
 makeBlack(Node Red l v r) = Node Black l v r
@@ -21,10 +24,11 @@ makeBlack t = t
 treeInsert :: (Ord a) => a -> Tree a -> Tree a
 treeInsert t x = makeBlack(insert t x)
 
-isElem :: (Ord a) => a -> Tree a -> Bool
-isElem x Nill = False
-isElem x (Node c left a right)
-  | x == a = True
-  | x < a = isElem x left
-  | x > a = isElem x right
+draw :: Tree a -> Int -> String
+draw Nill x = foldr (++) "" (take x $ repeat "\t") ++ "Nill\n"
+draw (Node Black r v l) x = (draw r (x+1)) ++ (foldr (++) "" (take x $ repeat "\t") ++ "B\n") ++ (draw l (x+1))
+draw (Node Red r v l) x = (draw r (x+1)) ++ (foldr (++) "" (take x $ repeat "\t") ++ "R\n") ++ (draw l (x+1))
+--draw (Node Black r v l) x = (draw r (x+1)) ++ ("R" ++ (show v)) ++ (draw l (x+1))
 
+drawTree :: Tree a -> IO ()
+drawTree t = putStr $ draw t 0 ++ "\n"
