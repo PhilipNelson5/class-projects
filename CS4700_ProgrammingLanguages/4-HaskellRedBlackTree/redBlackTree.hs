502a7020705da0d7653e3a413ebe43d4e151a5dc
Philip Nelson <PhilipNelson5@gmail.com>
2017-11-09 10:13:04 -0700

    readme and added height function

--- CS4700_ProgrammingLanguages/4-HaskellRedBlackTree/redBlackTree.hs
+++ CS4700_ProgrammingLanguages/4-HaskellRedBlackTree/redBlackTree.hs
@@ -1,18 +1,15 @@
 data Color = Red | Black deriving (Show)
 data Tree a = Nill | Node Color (Tree a) a (Tree a) deriving (Show)
 
-singleton :: a -> Tree a
-singleton x = Node Red Nill x Nill
-
 insert :: (Ord a) => a -> Tree a -> Tree a
-insert x Nill = singleton x
+insert x Nill = Node Red Nill x Nill
 insert x (Node c l v r)
   | x == v = Node c l v r
   | x < v  = balance $ Node c (insert x l) v r
   | x > v  = balance $ Node c l v (insert x r)
 
 balance :: Tree a -> Tree a
-balance (Node Black (Node Red (Node Red a x b) y c ) z d) = (Node Red (Node Black a x b) y (Node Black c z d))
+balance (Node Black (Node Red (Node Red a x b) y c) z d) = (Node Red (Node Black a x b) y (Node Black c z d))
 balance (Node Black (Node Red a x (Node Red b y c)) z d) = (Node Red (Node Black a x b) y (Node Black c z d))
 balance (Node Black a x (Node Red (Node Red b y c) z d)) = (Node Red (Node Black a x b) y (Node Black c z d))
 balance (Node Black a x (Node Red b y (Node Red c z d))) = (Node Red (Node Black a x b) y (Node Black c z d))
@@ -27,11 +24,15 @@ treeInsert t x = makeBlack(insert t x)
 
 draw :: Show a => Tree a -> Int -> String
 draw Nill x = tabs x ++ "Nill\n"
-draw (Node Black r v l) x = (draw l (x+1)) ++ (tabs x) ++ "B " ++ show v ++ "\n" ++ (draw r (x+1))
-draw (Node Red r v l) x = (draw l (x+1)) ++ (tabs x) ++ "R " ++ show v ++ "\n" ++ (draw r (x+1))
+draw (Node Black r v l) x = (draw l $ x+1) ++ (tabs x) ++ "B " ++ show v ++ "\n" ++ (draw r $ x+1)
+draw (Node Red r v l) x = (draw l $ x+1) ++ (tabs x) ++ "R " ++ show v ++ "\n" ++ (draw r $ x+1)
 
 tabs :: Int -> String
 tabs x = foldr (++) "" (take x $ repeat "\t")
 
-drawt :: Show a => Tree a -> IO ()
-drawt t = putStr $ draw t 0
+display :: Show a => Tree a -> IO ()
+display t = putStr $ draw t 0
+
+height :: (Ord a) => Tree a -> Int
+height Nill = 0
+height (Node _ l _ r) = max ((height l) + 1) ((height r) + 1) 
