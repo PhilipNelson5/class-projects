b3ed3bbf4211ff811763bd0817f327aea243d273
Philip Nelson <PhilipNelson5@gmail.com>
2017-11-16 11:25:39 -0700

    Mostly written, not compiling

--- CS4700_ProgrammingLanguages/5-ExpectedMonad/expected.hpp
+++ CS4700_ProgrammingLanguages/5-ExpectedMonad/expected.hpp
@@ -2,36 +2,89 @@
 #define EXPECTED_HPP
 
 #include <exception>
+#include <functional>
 #include <iostream>
 
 template <typename T>
 class Expected
 {
-  T data;
-  std::exception_ptr error;
+private:
+  union State{
+    T t;
+    std::exception_ptr e;
+
+  } data;
+
+  bool good;
 
 public:
-  Expected(T t) : data(t), error(nullptr) {}
+  Expected(T t) : data({.t = {t}}), good(true) {}
+  Expected(std::exception_ptr p) : data({.e = {p}}), good(false) {}
+  Expected(std::exception e) : data({.e = {std::make_exception_ptr(e)}}), good(false) {}
 
-  T value(void)
+  T value(void) const
   {
-    if (error)
-      throw error;
-    else
-      return data;
+    if (good) return data.t;
+    std::rethrow_exception(data.e);
+  }
+
+  operator T() { return value(); }
+
+  template <typename U>
+  Expected<U> apply(std::function<U(T)> f)
+  {
+    if (!good) return data.e;
+    try
+    {
+      return f(data.t);
+    }
+    catch (...)
+    {
+      return std::current_exception();
+    }
   }
 };
 
+// clang-format off
+#define MixedMode(op)\
+template <typename T, typename U>\
+auto op(Expected<T> t, U u)\
+{\
+  return t.apply([u](T t) { return op(t,u); });\
+}\
+\
+template <typename T, typename U>\
+auto op(T t, Expected<U> u)\
+{\
+  return t.apply([t](U u) { return op(t,u); });\
+}\
+// clang-format on
+
+MixedMode(operator+)
+MixedMode(operator-)
+MixedMode(operator*)
+MixedMode(operator/)
+MixedMode(operator%)
+MixedMode(operator>)
+MixedMode(operator>=)
+MixedMode(operator<)
+MixedMode(operator<=)
+MixedMode(operator==)
+
 template <typename T>
-std::ostream& operator<<(std::ostream& o, Expected<T> & e)
+std::ostream& operator<<(std::ostream& o, Expected<T>& e)
 {
   try
   {
     o << e.value();
   }
-  catch(std::exception const & e)
+  catch (std::exception const& e)
   {
     o << e.what();
+  }
+  catch (...)
+  {
+    o << "UNKNOWN ERROR";
   }
 
   return o;
--- CS4700_ProgrammingLanguages/5-ExpectedMonad/main.cpp
+++ CS4700_ProgrammingLanguages/5-ExpectedMonad/main.cpp
@@ -1,8 +1,11 @@
 #include "expected.hpp"
 #include <iostream>
+#include <exception>
 
 int main(void)
 {
-  Expected<int> a(2);
-  std::cout << a << std::endl;
+  Expected<int> a = 2;
+  Expected<int> b(3);
+  Expected<int> c = a + b;
+  //std::cout <<  << std::endl;
 }
