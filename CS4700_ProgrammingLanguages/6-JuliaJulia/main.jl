5c79f5f1e0c0eb9883cc5e4b318e413de3d10be8
Philip Nelson <PhilipNelson5@gmail.com>
2017-11-28 14:13:06 -0700

    starting to write the body

--- CS4700_ProgrammingLanguages/6-JuliaJulia/main.jl
+++ CS4700_ProgrammingLanguages/6-JuliaJulia/main.jl
@@ -35,3 +35,81 @@ function readConfig(fileName)
   Config(pixlesX, pixlesY, midX, midY, axisLenX, axisLenY, maxIters, color1, color2, outputFile, minX, maxX, minY, maxY, pxlSizeX, pxlSizeY, step_r, step_g, step_b)
 
 end
+
+function getIters(cfg)
+  //calculate the initial real and imaginary part of z, based on the pixel location and zoom and position values
+    newRe = 1.5 * (x - w / 2) / (0.5 * zoom * w) + moveX;
+    newIm = (y - h / 2) / (0.5 * zoom * h) + moveY;
+    int i;
+    //start the iteration process
+    for(i = 0; i < maxIterations; i++)
+    {
+      //remember value of previous iteration
+      oldRe = newRe;
+      oldIm = newIm;
+      //the actual iteration, the real and imaginary part are calculated
+      newRe = oldRe * oldRe - oldIm * oldIm + cRe;
+      newIm = 2 * oldRe * oldIm + cIm;
+      //if the point is outside the circle with radius 2: stop
+      if((newRe * newRe + newIm * newIm) > 4) break;
+    }
+    //use color model conversion to get rainbow palette, make brightness black if maxIterations reached
+    color = HSVtoRGB(ColorHSV(i % 256, 255, 255 * (i < maxIterations)));
+    //draw the pixel
+    pset(x, y, color);
+
+int countIterations(MandelbrotConfig cfg, int i, int j){
+    double xtemp = 0.0;
+    double x = 0.0;
+    double y = 0.0;
+    int iteration = 0;
+    double x0 = cfg.minX + j * cfg.pixelSize;
+    double y0 = cfg.maxY - i * cfg.pixelSize;
+    
+    while (((x*x + y*y) < 4) && (iteration < cfg.maxIterations)){
+              xtemp = x*x - y*y + x0;
+              y = 2*x*y + y0;
+              x = xtemp;
+              iteration = iteration + 1;
+    }
+    return iteration;
+}
+
+  //each iteration, it calculates: new = old*old + c, where c is a constant and old starts at current pixel
+  double cRe, cIm;           //real and imaginary part of the constant c, determinate shape of the Julia Set
+  double newRe, newIm, oldRe, oldIm;   //real and imaginary parts of new and old
+
+  //pick some values for the constant c, this determines the shape of the Julia Set
+  cRe = -0.7;
+  cIm = 0.27015;
+
+  //loop through every pixel
+  for(int y = 0; y < h; y++)
+  for(int x = 0; x < w; x++)
+  {
+    //calculate the initial real and imaginary part of z, based on the pixel location and zoom and position values
+    newRe = 1.5 * (x - w / 2) / (0.5 * zoom * w) + moveX;
+    newIm = (y - h / 2) / (0.5 * zoom * h) + moveY;
+    //i will represent the number of iterations
+    int i;
+    //start the iteration process
+    for(i = 0; i < maxIterations; i++)
+    {
+      //remember value of previous iteration
+      oldRe = newRe;
+      oldIm = newIm;
+      //the actual iteration, the real and imaginary part are calculated
+      newRe = oldRe * oldRe - oldIm * oldIm + cRe;
+      newIm = 2 * oldRe * oldIm + cIm;
+      //if the point is outside the circle with radius 2: stop
+      if((newRe * newRe + newIm * newIm) > 4) break;
+    }
+    //use color model conversion to get rainbow palette, make brightness black if maxIterations reached
+    color = HSVtoRGB(ColorHSV(i % 256, 255, 255 * (i < maxIterations)));
+    //draw the pixel
+    pset(x, y, color);
+  }
+  //make the Julia Set visible and wait to exit
+  redraw();
+  sleep();
+  return 0;
