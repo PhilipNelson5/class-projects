75bf05c776c8f4429b18a84c7bd240c037bcc6ff
Philip Nelson <PhilipNelson5@gmail.com>
2018-01-23 14:43:58 -0700

    move file

--- MATH5620_NumericalSolutionsOfDifferentialEquations/HW/1/eigen.py
+++ MATH5620_NumericalSolutionsOfDifferentialEquations/HW/1/eigen.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
 from sympy import *
-import math
 
-#init_printing()
+init_printing()
 
 A = Matrix([[3.4109, -0.2693, -1.0643],[1.5870, 1.5546, -5.5361],[0.2981, -0.2981, 1.2277]])
 I = Matrix([[1,0,0],[0,1,0],[0,0,1]])
-y = symbols('y')
+y = symbol('y')
+lamba = symbol('lamba')
 
-#pprint(A - y*I)
+pprint(I)
 
 eVals = A.eigenvals()
+
 eVects = A.eigenvects()
 
 #print(eVals)
@@ -22,17 +23,3 @@ for val in list(eVals.keys()):
 
 print("\nEigen Vectors:")
 pprint(eVects)
-
-
-
-print("\nEigen Value Matricies:")
-print("-----------------------------------------------------")
-#for val in list(eVals.keys()):
-for val in [math.pi, 1.0/3.0, math.e]:
-    eig = val#.evalf()
-    m = A-I*eig
-    print(eig)
-    pprint(m)
-    pprint(m.rref())
-    print("-----------------------------------------------------")
-
