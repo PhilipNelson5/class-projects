4638f2061b68f9bdd99e7aaef851c8256bdf5406
Philip Nelson <PhilipNelson5@gmail.com>
2018-01-25 13:34:21 -0700

    working initially

--- CS5410_GameDev/1-GameLoop/gameloop.js
+++ CS5410_GameDev/1-GameLoop/gameloop.js
@@ -1,98 +1,88 @@
-let prevTime = performance.now()
+let prevTime = performance.now();
 let quit = false;
 let fps = 0;
 
 let events = [];
 
-function makeEvent (n, i, c, f) {
-  event = {
-    name:n,
-    interval:i,
-    count:c,
-    exec:f,
-    elapsed:0
-  };
+function makeEvent(n, i, c, f) {
+  event = {name: n, interval: i, count: c, exec: f, elapsed: 0};
 
   return event;
 }
 
 avgFPS = {
-  arr:[],
-  total:0,
-  get avg() { return this.total/this.n; },
-  n:60,
-  next : function(newVal) {
+  arr: [],
+  total: 0,
+  get avg() {
+    return this.total / this.n;
+  },
+  n: 60,
+  next: function(newVal) {
     this.total -= this.arr.shift();
     this.total += newVal;
     this.arr.push(newVal);
   }
 }
 
-for(i = 0; i < avgFPS.n; ++i)
+for (i = 0; i < avgFPS.n; ++i)
   avgFPS.arr.push(0);
 
-showFPS = makeEvent(
-  "Show FPS",
-  100,
-  Infinity,
-  function(){
-    document.getElementById('fps').innerHTML="FPS: " + avgFPS.avg.toFixed(2);
-  });
+showFPS = makeEvent('Show FPS', 100, Infinity, function() {
+  document.getElementById('fps').innerHTML = 'FPS: ' + avgFPS.avg.toFixed(2);
+});
 
 events.push(showFPS);
 
 window.addEventListener('keydown', function(event) {
   // console.log(event.keyCode)
-  if(event.keyCode == 32)
-    quit = true;
+  if (event.keyCode == 32) quit = true;
 }, false);
 
 function register() {
   inputName = document.getElementById('name');
   name = document.getElementById('name').value;
-  inputName.value = "";
+  inputName.value = '';
 
-  inputInterval = document.getElementById('interval').value;
+  inputInterval = document.getElementById('interval');
   interval = document.getElementById('interval').value;
-  inputInterval.value = "";
+  inputInterval.value = '';
 
-  inputCount = document.getElementById('count').value;
+  inputCount = document.getElementById('count');
   count = document.getElementById('count').value;
-  inputCount.value = "";
+  inputCount.value = '';
 
-  console.log("New Event: " + name + " " + interval + " " + count);
+  console.log('New Event: ' + name + ' ' + interval + ' ' + count);
 
-  event = makeEvent(
-    name,
-    interval,
-    count,
-    function(){console.log("Event: " + this.name + ": " + this.count);});
+  event = makeEvent(name, interval, count, function() {
+    let log = document.getElementById("log");
+    let output = document.getElementById("output");
+    log.innerText = log.innerText + '\nEvent:\t' + this.name + ':\t' + this.count;
+    output.scrollTop = output.scrollHeight;
+    --this.count;
+    this.elapsed = 0;
+  });
 
   events.push(event);
 }
 
-function processInput(dTime){
-}
+function processInput(dTime) {}
 
-function update(dTime){
-  fps = 1/dTime*1000;
-  avgFPS.next(fps);
+function update(dTime) {
+  fps = 1 / dTime * 1000;  // calculate fps
+  avgFPS.next(fps);        // update moving fps average
   events.forEach(function(e) {
-    if(e.elapsed >= e.interval)
-    {
-      e.elapsed=0;
-      --e.count;
-      e.exec();
-      if (e.count == 0)
-        events.splice(events.indexOf(e),1);
-    }
-    else
-      e.elapsed+=dTime;
+    if (e.count == 0)
+      events.splice(events.indexOf(e), 1);  // remove finished events
+    e.elapsed += dTime;                     // increment time
   });
 }
 
-function render(dTime){
-  //console.log(dTime);
+function render(dTime) {
+  events.forEach(function(e) {
+    if (e.elapsed >= e.interval) {
+      e.exec();
+    }
+  });
 }
 
 function gameloop() {
@@ -104,6 +94,5 @@ function gameloop() {
   update(dTime);
   render(dTime);
 
-  if(!quit)
-    requestAnimationFrame(gameloop);
+  if (!quit) requestAnimationFrame(gameloop);
 }
